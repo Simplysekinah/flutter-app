28d45391db3220819b9b16c2df131b5bf9b801e9
Simplysekinah <akinolasekinat123@gmail.com>
2025-05-06 04:38:31 -0700

    pushing

--- lib/Pages/tag.dart
+++ lib/Pages/tag.dart
@@ -1,4 +1,6 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:onboard/functiondio.dart';
 
 class TagPage extends StatefulWidget {
   const TagPage({super.key});
@@ -7,9 +9,105 @@ class TagPage extends StatefulWidget {
   State<TagPage> createState() => _TagPageState();
 }
 
+// List count = [];
+
 class _TagPageState extends State<TagPage> {
+  List recipes = [];
+
+  @override
+  void initState() {
+    super.initState();
+    loadRecipes();
+  }
+
+  Future<void> loadRecipes() async {
+    try {
+      Map<String, dynamic> prod = await result(); // Your API
+      setState(() {
+        recipes = prod['data'] ?? [];
+      });
+    } catch (e) {
+      print('Error fetching recipes: $e');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        // appBar: AppBar(title: Text('HomePage'), centerTitle: true),
+        body: SingleChildScrollView(
+            child: Container(
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height,
+      color: Colors.white,
+      child: Column(
+        children: [
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            decoration: BoxDecoration(
+              color: Colors.grey[200],
+              borderRadius: BorderRadius.circular(30),
+            ),
+            child: Row(
+              children: const [
+                Icon(Icons.search),
+                SizedBox(width: 8),
+                Expanded(
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: 'Search any recipes of your choice...',
+                      border: InputBorder.none,
+                    ),
+                  ),
+                ),
+                Icon(Icons.tune), // filter icon
+              ],
+            ),
+          ),
+          SizedBox(height: 50),
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height * 0.5,
+            child: ListView.builder(
+              itemCount: recipes.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  leading: CachedNetworkImage(
+                    imageBuilder: (context, imageProvider) => Container(
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: imageProvider,
+                          fit: BoxFit.cover,
+                          colorFilter: ColorFilter.mode(
+                            Colors.red,
+                            BlendMode.colorBurn,
+                          ),
+                        ),
+                      ),
+                    ),
+                    errorWidget: (context, url, error) => Icon(Icons.error),
+                    imageUrl: '${recipes[index]['image']}',
+                    height: 30,
+                    width: 30,
+                    progressIndicatorBuilder: (
+                      context,
+                      url,
+                      progress,
+                    ) {
+                      return CircularProgressIndicator(
+                        value: progress.progress,
+                      );
+                    },
+                  ),
+                  trailing: Icon(Icons.arrow_forward_ios_rounded),
+                  title: Text(recipes[index]['name']),
+                  subtitle: Text(recipes[index]['tags'].toString() ?? ''),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    )));
   }
 }
